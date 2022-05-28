803af4e30c3f61ff449da77cc471b877bea7786f
bheema-284 <bheemudug@gmail.com>
2022-05-28 21:34:51 +0530

    circles are added to the empty div

--- my_app/src/components/Circles.jsx
+++ my_app/src/components/Circles.jsx
@@ -1,35 +1,62 @@
 import '../App.css';
-import { useState } from 'react';
-export const Circles = () => {
-  const [value, setValue] = useState('');
-  const [visible, toggleVisible] = useState(false);
-
-  return (
-    <div className="div">
-      <div className="ediv"></div>
+  import { useState } from 'react';
+  export const Circles = () => {
+    const [value, setValue] = useState('');
+    const [visible, toggleVisible] = useState([]);
+    const circles = [
+      { id: 1, color: 'rgb(94, 197, 94)' },
+      { id: 2, color: 'rgb(236, 236, 102)' },
+      { id: 3, color: 'rgb(107, 107, 229)' },
+      { id: 4, color: 'rgb(162, 241, 241)' },
+      { id: 5, color: 'rgb(244, 192, 244)' },
+    ];
+    return (
       <div>
-        5 Circles
-        <div
-          onClick={() => toggleVisible(!visible)}
-          id="1"
-          className="one"></div>
-        <div
-          id="2"
-          onClick={() => toggleVisible(!visible)}
-          className="two"></div>
-        <div
-          id="3"
-          onClick={() => toggleVisible(!visible)}
-          className="three"></div>
-        <div
-          id="4"
-          onClick={() => toggleVisible(!visible)}
-          className="four"></div>
-        <div
-          id="5"
-          onClick={() => toggleVisible(!visible)}
-          className="five"></div>
+        <input
+          type="number"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            if ([1, 2, 3, 4, 5].includes(+value)) {
+              toggleVisible([...visible, Number(value)]);
+            }
+          }}>
+          Shoot
+        </button>
+        <div className="box">
+          {circles.map((e) =>
+            visible.includes(e.id) ? (
+              <div
+                onClick={(el) => {
+                  const filter = visible.filter((el) => el !== e.id);
+                  console.log(filter);
+                  toggleVisible(filter);
+                }}
+                style={{ background: e.color }}
+                className="balls">
+                {e.id}
+              </div>
+            ) : null,
+          )}
+        </div>
+        <div className="box">
+          {circles.map((e) =>
+            !visible.includes(e.id) ? (
+              <div
+                onClick={(el) => {
+                  toggleVisible([...visible, e.id]);
+                  console.log(visible);
+                }}
+                style={{ background: e.color }}
+                className="balls">
+                {e.id}
+              </div>
+            ) : null,
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
