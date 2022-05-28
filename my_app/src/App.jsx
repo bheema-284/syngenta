4f0e8227e909b9917f1a4256aed5ae4b2a33c5d5
bheema-284 <bheemudug@gmail.com>
2022-05-28 21:56:43 +0530

    navbar is added

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { Circles } from './components/Circles';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Circles />
     </div>
   );
---
+++ my_app/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import '../App.css';
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h3 className="h3">Syngenta Assignment</h3>
+    </div>
+  );
+};
