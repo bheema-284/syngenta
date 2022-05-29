18a2b6eadf34b6c003ab24abcbd9fd8bcac383f1
Bheemudu Guguloth <96168073+bheema-284@users.noreply.github.com>
2022-05-29 08:34:36 +0530

    Update db.js

--- backend/src/Configs/db.js
+++ backend/src/Configs/db.js
@@ -2,6 +2,7 @@ const { mongoose } = require('mongoose');
 
 module.exports = () => {
   return mongoose.connect(
-    'mongodb+srv://bheema:bheema_123@cluster0.2da72.mongodb.net/?retryWrites=true&w=majority',
+  'mongodb+srv://bheema:bheema_123@cluster0.ajvpj.mongodb.net/syngenta'  ,
   );
 };
+// 'mongodb+srv://bheema:bheema_123@cluster0.2da72.mongodb.net/?retryWrites=true&w=majority'
